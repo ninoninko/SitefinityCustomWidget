eab0d1fd0e9cf7e0315d5bd11c5645171018794a
Yanaki Krastev <82317739+ninoninko@users.noreply.github.com>
2022-11-07 17:10:13 +0200

    Add the main code logic of the SpaceX API widget
    In the following files we can find everything for the SpaceX API widget. We have 3 .cshtml files which are the different pages of the widget and .cs files which are reading from the api and calling the respective .cshtml-s.

---
+++ Mvc/ViewModels/LaunchViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpaceXWidget.Mvc.ViewModels
+{
+    public class LaunchViewModel
+    {
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "launchpad")]
+        public string LaunchpadNumber { get; set; }
+
+        [JsonProperty(PropertyName = "links")]
+        public LinkViewModel Links { get; set; }
+
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+    }
+
+    public class LinkViewModel
+    {
+        public PatchViewModel Patch { get; set; }
+
+        [JsonProperty(PropertyName = "webcast")]
+        public string VideoToWatch { get; set; }
+    }
+
+  
+    public class PatchViewModel
+    {
+        [JsonProperty(PropertyName = "large")]
+        public string Large { get; set; }
+
+        [JsonProperty(PropertyName = "small")]
+        public string Small { get; set; }
+    }
+}
